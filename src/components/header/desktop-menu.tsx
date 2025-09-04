7f73cd112edd25885ac4ce6589fe547b8db3c952
Gabrielk127 <gabrielfsilva09@gmail.com>
2025-09-04 10:43:25 -0300

    fix: create button hidden

--- src/components/header/desktop-menu.tsx
+++ src/components/header/desktop-menu.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import CreatePropertyButton from "../CreatePropertyButton";
+// import CreatePropertyButton from "../CreatePropertyButton";
 
 interface DesktopMenuProps {
   hasCondominium?: boolean;
@@ -39,7 +39,7 @@ const DesktopMenu = ({ hasCondominium = true, hasVideo = true }: DesktopMenuProp
             <Image src="/logo-branco.png" width={200} height={200} alt="Logo da ax" />
           </Link>
         </motion.div>
-        <CreatePropertyButton />
+        {/* <CreatePropertyButton /> */}
 
         {/* Menu Links */}
         <nav className="hidden md:flex space-x-8">
